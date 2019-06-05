6a04fc0662392ddeda7e4cd56a0ccda84cbd7e07
Dan Hausner <dn.hausner@gmail.com>
2019-06-05 16:51:08 +0100

    LoRaWAN uplink example

---
+++ sandbox/LoRaWAN_OTAA/main.py
@@ -0,0 +1,52 @@
+from network import LoRa
+import socket
+import time
+import ubinascii
+from keys import APP_EUI, APP_KEY
+
+# Initialise LoRa in LORAWAN mode.
+# Please pick the region that matches where you are using the device:
+# Asia = LoRa.AS923
+# Australia = LoRa.AU915
+# Europe = LoRa.EU868
+# United States = LoRa.US915
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+
+# create an OTAA authentication parameters
+app_eui = ubinascii.unhexlify(APP_EUI)
+app_key = ubinascii.unhexlify(APP_KEY)
+
+# join a network using OTAA (Over the Air Activation)
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+
+# wait until the module has joined the network
+while not lora.has_joined():
+    time.sleep(2.5)
+    print('Not yet joined...')
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+
+# make the socket blocking
+# (waits for the data to be sent and for the 2 receive windows to expire)
+s.setblocking(True)
+
+# send some data
+for i in range(255):
+    print("Sending:", i)
+    s.send(bytes([i]))
+    print("Sleeping")
+    time.sleep(1)
+
+# s.send(bytes([0x01, 0x02, 0x03]))
+
+# make the socket non-blocking
+# (because if there's no data received it will block forever...)
+s.setblocking(False)
+
+# get any data received (if any...)
+data = s.recv(64)
+print(data)
