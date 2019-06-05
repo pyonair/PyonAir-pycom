ccbb7d9e72ea6e921aa8577229da52a93125e52b
Dan Hausner <dn.hausner@gmail.com>
2019-06-05 16:58:39 +0100

    Convert received message to int

--- sandbox/MQTT_Client/mqtt_client.py
+++ sandbox/MQTT_Client/mqtt_client.py
@@ -1,6 +1,7 @@
 # Python 3.7
 import time
 import ttn
+import base64
 
 import sys
 sys.path.append('../..')
@@ -9,7 +10,10 @@ from keys import app_id, access_key
 
 def uplink_callback(msg, client):
     print("Received uplink from ", msg.dev_id)
-    print(msg)
+    print("msg:", msg)
+    print("raw payload:", msg.payload_raw)
+    print("encoded payload:", int(base64.b64decode(msg.payload_raw).hex()))
+    print()
 
 
 handler = ttn.HandlerClient(app_id, access_key)
@@ -20,6 +24,6 @@ mqtt_client = handler.data()
 mqtt_client.set_uplink_callback(uplink_callback)
 mqtt_client.connect()
 print("Going to sleep")
-time.sleep(60)
+time.sleep(10*60)
 print("Closing the connection")
 mqtt_client.close()
