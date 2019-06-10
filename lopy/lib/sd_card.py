9db571356b15570da69712bcfe4a1c91164547a4
Dan Hausner <dn.hausner@gmail.com>
2019-06-10 13:34:47 +0100

    Create log file with headers

--- lopy/lib/sd_card.py
+++ lopy/lib/sd_card.py
@@ -1,5 +1,8 @@
 from machine import SD
 import os
+from strings import headers_dict_v3
+
+headers = headers_dict_v3['PMS5003']
 
 
 class SDCard:
@@ -8,6 +11,10 @@ class SDCard:
         self.path_template = '/sd/{}'
         os.mount(self.sd, '/sd')
 
+    def create_log_file(self, filename):
+        with open(self.path_template.format(filename), 'w') as f:  # TODO: Change permission to x in production
+            f.write(', '.join(headers) + '\n')
+
     def write_line(self, filename, line):
-        with open(self.path_template.format(filename), 'w') as f:
+        with open(self.path_template.format(filename), 'a') as f:
             f.write(line + '\n')
