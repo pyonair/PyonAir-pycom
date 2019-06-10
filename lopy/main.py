b0238bb22370dc5be2b56ca48735634cc8774408
Dan Hausner <dn.hausner@gmail.com>
2019-06-10 15:05:12 +0100

    Log measurements and errors in 2 different files

--- lopy/main.py
+++ lopy/main.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 from plantower import Plantower, PlantowerException
 import time
+from sd_card import SDCard
+from machine import RTC
+from strings import log_file_template, timestamp_template
+
+# Initialise the time
+rtc = RTC()
+rtc.init((2017, 2, 28, 10, 30, 0, 0, 0)) # TODO: get time from GPS
+
+# Initialise SD card
+sd = SDCard()
+
+# Create log file with headers
+now = rtc.now()
+log_file = '{}.csv'.format(log_file_template.format(*now))
+sd.create_log_file(log_file)
 
 plantower = Plantower()
 
@@ -10,6 +25,9 @@ while (True):
         if recv:
             print(recv)
             print()
-    except PlantowerException as pe:
-        print(pe)
+            sd.write_line(log_file, str(recv))
+    except PlantowerException as e:
+        error_line = ', '.join([timestamp_template.format(*rtc.now()), str(e.__class__)])
+        print(error_line)
+        sd.write_line('error_log.txt', error_line)
     time.sleep(0.1)
