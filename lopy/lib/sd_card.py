d13aea07596e611202e5cb3faef12f8dd93fd0b3
Dan Hausner <dn.hausner@gmail.com>
2019-06-10 12:00:26 +0100

    Delete prints

--- lopy/lib/sd_card.py
+++ lopy/lib/sd_card.py
@@ -4,12 +4,9 @@ import os
 
 class SDCard:
     def __init__(self):
-        print("1")
         self.sd = SD()
-        print("2")
         self.path_template = '/sd/{}'
         os.mount(self.sd, '/sd')
-        print("3")
 
     def write_line(self, filename, line):
         with open(self.path_template.format(filename), 'w') as f:
