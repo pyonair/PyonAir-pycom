50c6b2b7f6d835e03cddf473e3185fe6b8b3f08f
Dan Hausner <dn.hausner@gmail.com>
2019-06-17 13:09:46 +0100

    Move filename and error filename inside the SDCard class

--- lopy/lib/sd_card.py
+++ lopy/lib/sd_card.py
@@ -6,15 +6,24 @@ headers = headers_dict_v3['PMS5003']
 
 
 class SDCard:
-    def __init__(self):
+    def __init__(self, logfile, error_logfile=None):
         self.sd = SD()
         self.path_template = '/sd/{}'
         os.mount(self.sd, '/sd')
+        self.logfile = logfile
+        self.error_logfile = error_logfile
 
     def create_log_file(self, filename):
         with open(self.path_template.format(filename), 'w') as f:  # TODO: Change permission to x in production
             f.write(', '.join(headers) + '\n')
 
-    def write_line(self, filename, line):
-        with open(self.path_template.format(filename), 'a') as f:
+    def log_line(self, line):
+        with open(self.path_template.format(self.logfile), 'a') as f:
             f.write(line + '\r\n')
+
+    # Setters
+    def set_logfile(self, logfile):
+        self.logfile = logfile
+
+    def set_error_logfile(self, error_logfile):
+        self.error_logfile = error_logfile
--- lopy/main.py
+++ lopy/main.py
@@ -7,15 +7,19 @@ from strings import log_file_template, timestamp_template
 
 # Initialise the time
 rtc = RTC()
-rtc.init((2017, 2, 28, 10, 30, 0, 0, 0)) # TODO: get time from GPS
+rtc.init((2017, 2, 28, 10, 30, 0, 0, 0))  # TODO: get time from GPS
+now = rtc.now()
 
 # Initialise SD card
-sd = SDCard()
+logfile = '{}.csv'.format(log_file_template.format(*now))
+error_logfile = 'error_log.txt'
+sd = SDCard(
+    logfile=logfile,
+    error_logfile=error_logfile
+)
 
 # Create log file with headers
-now = rtc.now()
-log_file = '{}.csv'.format(log_file_template.format(*now))
-sd.create_log_file(log_file)
+sd.create_log_file(logfile)
 
 plantower = Plantower()
 
@@ -25,9 +29,9 @@ while (True):
         if recv:
             print(recv)
             print()
-            sd.write_line(log_file, str(recv))
+            sd.log_line(str(recv))
     except PlantowerException as e:
         error_line = ', '.join([timestamp_template.format(*rtc.now()), str(e.__class__)])
         print(error_line)
-        sd.write_line('error_log.txt', error_line)
+        sd.log_line(error_line)
     time.sleep(0.1)
