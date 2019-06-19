924c7f680f4b3dc97fe979fe94f52ad9ac6b0550
Dan Hausner <dn.hausner@gmail.com>
2019-06-19 13:43:18 +0100

    Feat, compute and log an average reading for when there is more than 1 reading in a second

--- lopy/main.py
+++ lopy/main.py
@@ -4,6 +4,7 @@ import time
 from sd_card import SDCard
 from machine import RTC, Timer
 from strings import sensor_logfile_template, timestamp_template
+from helper import mean_across_arrays
 
 # Setting
 loop_time = 0.1  # delay between the sensor readings (seconds)
@@ -21,22 +22,41 @@ sd = SDCard(
     debug=True
 )
 
+# Global variables for sensor reading and computing averages
 plantower = Plantower()
+last_timestamp = None
+sensor_readings_lst = []
 
 # Chronometer for timing the loop execution
 chrono = Timer.Chrono()
 chrono.start()
-while (True):
+while True:
+
     try:
         recv = plantower.read()
         if recv:
             print(recv)
             print()
-            sd.log_sensor_line(str(recv))
+            recv_lst = str(recv).split(',')
+            curr_timestamp = recv_lst[0]
+            sensor_reading = [int(i) for i in recv_lst[1:]]
+            if curr_timestamp != last_timestamp:
+                # If there are any readings with the previous timestamps, process them
+                if len(sensor_readings_lst) > 0:
+                    lst_to_log = [last_timestamp] + [str(i) for i in mean_across_arrays(sensor_readings_lst)]
+                    line_to_log = ','.join(lst_to_log)
+                    sd.log_sensor_line(line_to_log)
+                # Set/reset global variables
+                last_timestamp = curr_timestamp
+                sensor_readings_lst = []
+            # Add the current reading to the list, which will be processed when the timestamp changes
+            sensor_readings_lst.append(sensor_reading)
+
     except PlantowerException as e:
         status_line = ', '.join([timestamp_template.format(*rtc.now()), str(e.__class__)])
         print(status_line)
         # TODO: log exception
+
     finally:
         elapsed = chrono.read()
         if elapsed < loop_time:
