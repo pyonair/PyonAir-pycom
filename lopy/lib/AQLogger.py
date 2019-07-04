1eb43027efb6d6c3d5b4b7f31e3c13278260f1db
Dan Hausner <dn.hausner@gmail.com>
2019-07-04 12:02:18 +0100

    Rename sensor_logger to status_logger

--- lopy/lib/AQLogger.py
+++ lopy/lib/AQLogger.py
@@ -1,30 +1,29 @@
 import loggingpycom as logging
 from loggingpycom import handlers
 
-SENSOR_FMT_DEFAULT = '%(asctime)s - %(name)s - %(message)s'
-SENSOR_MAX_FILE_SIZE_DEFAULT = 10 * 1024 * 1024  # 10MiB
-SENSOR_ARCHIVE_COUNT_DEFAULT = 10  # How many files to keep before deletion
+STATUS_FMT_DEFAULT = '%(levelname)s - %(asctime)s - %(name)s - %(message)s'
+STATUS_MAX_FILE_SIZE_DEFAULT = 10 * 1024 * 1024  # 10MiB
+STATUS_ARCHIVE_COUNT_DEFAULT = 10  # How many files to keep before deletion
 
 
 class AQLogger():
     def __init__(
             self,
             level=logging.INFO,
-            sensor_fmt=SENSOR_FMT_DEFAULT
     ):
         self.level = level
         self.loggers = {}  # dictionary to store loggers
 
-    def create_sensor_logger(
+    def create_status_logger(
             self,
             name,
-            fmt=SENSOR_FMT_DEFAULT,
+            fmt=STATUS_FMT_DEFAULT,
             filename=None,
-            maxBytes=SENSOR_MAX_FILE_SIZE_DEFAULT,
-            backupCount=SENSOR_ARCHIVE_COUNT_DEFAULT
+            maxBytes=STATUS_MAX_FILE_SIZE_DEFAULT,
+            backupCount=STATUS_ARCHIVE_COUNT_DEFAULT
     ):
         """
-        Create sensor logger and add it to the self.loggers dictionary
+        Create status logger and add it to the self.loggers dictionary
         :param name:
         :type name: str
         :param fmt: format string for the logger
@@ -36,13 +35,13 @@ class AQLogger():
         :param backupCount: number of archive files
         :type backupCount: int
         """
-        sensor_logger = logging.getLogger(name)
+        status_logger = logging.getLogger(name)
         formatter = logging.Formatter(fmt=fmt)
         sh = logging.StreamHandler()  # handler for printing to terminal
         sh.setFormatter(formatter)
-        sensor_logger.addHandler(sh)
+        status_logger.addHandler(sh)
         if filename:
             file_handler = handlers.RotatingFileHandler(filename, maxBytes=maxBytes, backupCount=backupCount)
             file_handler.setFormatter(formatter)
-            sensor_logger.addHandler(file_handler)
-        self.loggers[name] = sensor_logger
+            status_logger.addHandler(file_handler)
+        self.loggers[name] = status_logger
