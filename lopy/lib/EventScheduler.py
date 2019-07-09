0aec82dd7d99354fd16d890601aa6d441b85ca3f
pe-varga <52164948+pe-varga@users.noreply.github.com>
2019-07-09 18:07:22 +0100

    Refactor - EventScheduler and configuration
    Refactored the event scheduler to make it more clean and generic. Now it can be used to create event queues for more than one sensor.
    
    Moved all the tasks from the event scheduler to tasks.py so taking averages and sending data over lora can be evoked from any file.
    
    Refactored the configuration to make it cleaner and include reboot upon successful configuration.
    
    The config file is now red in the main upon booting.

--- lopy/lib/EventScheduler.py
+++ lopy/lib/EventScheduler.py
@@ -1,39 +1,51 @@
 import machine
 from machine import RTC, Timer
-from tasks import flash_pm_averages
-import _thread
-from LoRa_thread import lora_thread
+from tasks import flash_pm_averages, send_over_lora
+from helper import seconds_to_first_event
 
 
 class EventScheduler:
-    def __init__(self, avg_interval, rtc, sensor_name, logger):
+    def __init__(self, interval_s, rtc, sensor_name, logger):
 
-        self.avg_interval_s = avg_interval
+        #  Arguments
+        self.interval_s = interval_s
         self.rtc = rtc
         self.logger = logger
         self.sensor_name = sensor_name
+
+        #  Attributes
         self.s_to_next_lora = None
-        self.logger = logger
+        self.first_alarm = None
+        self.periodic_alarm = None
+        self.random_alarm = None
 
-        self.count = 1
-        self.now = rtc.now()
-        self.next_event_s = self.avg_interval_s - (
-                    ((self.now[3] * 3600) + (
-                     self.now[4] * 60) + self.now[5] + (self.now[6]) / 1000000) % self.avg_interval_s)
-        self.periodic_average_event = None
-        self.random_lora_event = None
-        self.first_event = Timer.Alarm(self.set_event_queue, s=self.next_event_s, periodic=False)
+        #  Start scheduling events
+        self.set_event_queue()
 
-    def set_event_queue(self, arg):
-        self.periodic_average_event = Timer.Alarm(self.get_averages, s=self.avg_interval_s, periodic=True)
-        self.get_averages(arg)
-
-    def get_averages(self, arg):
-        self.logger.info("Running flash_pm_averages task")
+    #  Calculates time (s) until the first event, and sets up an alarm
+    def set_event_queue(self):
+        #  process and send previous data immediately upon boot
         flash_pm_averages(sensor_name=self.sensor_name, logger=self.logger)
-        self.s_to_next_lora = int(machine.rng() / (2**24) * self.avg_interval_s)
-        self.random_lora_event = Timer.Alarm(self.send_over_lora, s=self.s_to_next_lora, periodic=False)
+        send_over_lora(sensor_name=self.sensor_name, logger=self.logger, timeout=60)
+        first_event_s = seconds_to_first_event(self.rtc, self.interval_s)
+        self.first_alarm = Timer.Alarm(self.first_event, s=first_event_s, periodic=False)
 
-    def send_over_lora(self, arg):
-        self.logger.info("Sending data over LoRaWAN")
-        _thread.start_new_thread(lora_thread, ('LoRa_send', "PM1.csv.tosend", self.logger, 30))
+    def first_event(self, arg):
+        #  set up periodic alarm with specified interval to calculate averages
+        self.periodic_alarm = Timer.Alarm(self.periodic_event, s=self.interval_s, periodic=True)
+        self.periodic_event(arg)
+
+    def periodic_event(self, arg):
+        self.logger.info("Running flash_pm_averages task")
+
+        #  flash averages of data to sd card at the end of the interval
+        flash_pm_averages(sensor_name=self.sensor_name, logger=self.logger)
+
+        #  get random number of seconds within interval
+        self.s_to_next_lora = int(machine.rng() / (2**24) * self.interval_s)
+
+        #  set up an alarm with random delay to send data over LoRa
+        self.random_alarm = Timer.Alarm(self.random_event, s=self.s_to_next_lora, periodic=False)
+
+    def random_event(self, arg):
+        send_over_lora(sensor_name=self.sensor_name, logger=self.logger, timeout=60)
--- lopy/lib/configuration.py
+++ lopy/lib/configuration.py
@@ -2,102 +2,153 @@ import network
 from network import WLAN
 import usocket as socket
 from html import html_form, html_acknowledgement
+import machine
 import time
 import pycom
 import gc
 from strings import config_filename
 import os
+import _thread
+
+config_lock = _thread.allocate_lock()
 
 
-def request_credentials(sd, sct, logger):
+def config_thread(id, logger, timeout):
+    """
+    Thread that turns on access point on the device to modify configurations. Name of access point: PmSensor
+    Password: pmsensor Enter 192.168.4.10 on device browser to get configuration form. Indicator LEDs:
+    Blue - Access point turned on, not connected, Green - Connected, configuration is open on browser,
+    Red - An error has occured
+    The device automatically reboots and applies modifications upon successful configuration.
+    :param id: Thread id
+    :type id: str
+    :param logger: status logger
+    :type logger: LoggerFactory object
+    :param timeout: timeout (in seconds) for user configuration
+    :type timeout: int
+    """
 
-    index = [None] * 4
-    credentials = {0: 'APP_KEY', 1: 'APP_EUI', 2: 'interval'}
+    #  Only one of this thread is allowed to run at a time
+    if not config_lock.locked():
+        with config_lock:
 
+            logger.info("Thread: {} started".format(id))
+
+            # set pycom up as access point
+            wlan = network.WLAN(mode=WLAN.AP, ssid='PmSensor')
+            # Connect to PmSensor using password pmsensor
+            wlan.init(mode=WLAN.AP, ssid='PmSensor', auth=(WLAN.WPA2, 'pmsensor'), channel=1, antenna=WLAN.INT_ANT)
+            # Load HTML via entering 192,168.4.10 to your browser
+            wlan.ifconfig(id=1, config=('192.168.4.10', '255.255.255.0', '192.168.4.1', '192.168.4.1'))
+
+            logger.info('Access point turned on as PmSensor')
+            logger.info('Configuration website can be accessed at 192.168.4.10')
+
+            address = socket.getaddrinfo('0.0.0.0', 80)[0][-1]  # Accept stations from all addresses
+            sct = socket.socket()  # Create socket for communication
+            sct.settimeout(timeout)  # session times out after x seconds
+            gc.collect()  # frees up unused memory if there was a previous connection
+            sct.bind(address)  # Bind address to socket
+            sct.listen(1)  # Allow one station to connect to socket
+
+            pycom.heartbeat(False)
+            pycom.rgbled(0x0000FF)  # Blue LED - Initialized, waiting for connection
+
+            reboot = False
+
+            if get_configuration(sct, logger):
+                reboot = True
+
+            wlan.deinit()  # turn off wifi
+            pycom.heartbeat(True)  # disable indicator LEDs
+            gc.collect()
+
+            if reboot:
+                logger.info('rebooting...')
+                machine.reset()
+
+
+#  Sends html form over wifi and receives data from the user
+def get_configuration(sct, logger):
     try:
         while True:
-            # print('Waiting for new connection')
-            client, address = sct.accept()
-            # print('Client connected at: ', address)
+            client, address = sct.accept()  # wait for new connection
             client.send(html_form)  # send html page with form to submit by the user
             pycom.rgbled(0x00FF00)  # Green LED - Connection successful
             received_data = str(client.recv(3000))  # wait for client response
-            print("received data:", received_data)
-            for i in range(0, 3):  # find indices in received message
-                index[i] = received_data.rfind(credentials[i])
-            if -1 not in index[0:3]:  # if all keys were found in the message, cut them up to strings
-                APP_KEY = received_data[(index[0] + 8):index[1] - 1]
-                print("APP_KEY:", APP_KEY)
-                APP_EUI = received_data[(index[1] + 8):index[2] - 1]
-                print("APP_EUI:", APP_EUI)
-                index[3] = received_data.find("\r", index[2])
-                if index[3] != -1:  # last key is not in the end of the message
-                    interval = received_data[(index[2] + 9):index[3]]
-                    print("interval: {} minutes".format(interval))
-                else:  # last key is in the end of the message
-                    interval = received_data[(index[2] + 9):-1]
-                    print("interval: {} minutes".format(interval))
-                client.send(html_acknowledgement.format(APP_KEY, APP_EUI, interval))  # sends acknowledgement to user
-                client.close()
-                logger.info('Configuration data received from user')
-                try:
-                    with open('/sd/config.txt', 'w') as f:  # save credentials to sd card
-                        f.write(APP_KEY + '\r\n' + APP_EUI + '\r\n' + interval + '\r\n')
-                    logger.info('Configuration saved to SD card')
-                    get_config(logger)  # Load new configuration from sd card
-                except:
-                    logger.error('Could not save config file')
-                finally:
-                    return
+            #  print("received data:", received_data)
+            if process_data(client, received_data, logger):
+                return True
     except:
         pycom.rgbled(0xFF0000)  # Red LED - Connection timeout
         logger.warning('Wifi configuration session timed out')
         time.sleep(3)
-        return
+        return False
 
 
-def config_thread(sd, id, logger):
+#  Parses keys and preferences from data received
+def process_data(client, received_data, logger):
 
-    print("Thread: {} started".format(id))
+    index = [None] * 4
+    config_names = {0: 'APP_KEY', 1: 'APP_EUI', 2: 'interval'}
 
-    # set pycom up as access point
-    wlan = network.WLAN(mode=WLAN.AP, ssid='MVP')
-    # Connect to MVP using the passowrd pmsensor
-    wlan.init(mode=WLAN.AP, ssid='MVP', auth=(WLAN.WPA2, 'pmsensor'), channel=1, antenna=WLAN.INT_ANT)
-    # Load HTML via entering 192,168.4.10 to your browser
-    wlan.ifconfig(id=1, config=('192.168.4.10', '255.255.255.0', '192.168.4.1', '192.168.4.1'))
+    for i in range(0, 3):  # find indices in received message
+        index[i] = received_data.rfind(config_names[i])
 
-    print('Access point turned on as MVP')
-    print('Configuration website can be accessed at 192.168.4.10')
+    if -1 not in index[0:3]:  # if all keys were found in the message, cut them up to strings
+        APP_KEY = received_data[(index[0] + 8):index[1] - 1]
+        logger.info('APP_KEY: {}'.format(APP_KEY))
+        APP_EUI = received_data[(index[1] + 8):index[2] - 1]
+        logger.info('APP_EUI: {}'.format(APP_EUI))
+        index[3] = received_data.find("\r", index[2])
+        if index[3] != -1:  # last key is not in the end of the message
+            interval = received_data[(index[2] + 9):index[3]]
+            logger.info("interval: {} minutes".format(interval))
+        else:  # last key is in the end of the message
+            interval = received_data[(index[2] + 9):-1]
+            logger.info("interval: {} minutes".format(interval))
 
-    address = socket.getaddrinfo('0.0.0.0', 80)[0][-1]  # Accept stations from all addresses
-    sct = socket.socket()  # Create socket for communication
-    sct.settimeout(300)  # session times out after x seconds
-    gc.collect()  # frees up unused memory if there was a previous connection
-    sct.bind(address)  # Bind address to socket
-    sct.listen(1)  # Allow one station to connect to socket
+        client.send(html_acknowledgement.format(APP_KEY, APP_EUI, interval))  # sends acknowledgement to user
+        client.close()
+        logger.info('Configuration data received from user')
 
-    pycom.heartbeat(False)
-    pycom.rgbled(0x0000FF)  # Blue LED - Initialized, waiting for connection
+        config_values = {'APP_KEY': APP_KEY, 'APP_EUI': APP_EUI, 'interval': interval}
 
-    request_credentials(sd, sct, logger)
+        if save_configuration(logger, config_values):
+            return True
+        else:
+            logger.error('Could not save config file')
+            raise Exception('Could not save config file')
 
-    wlan.deinit()  # turn off wifi
-    pycom.heartbeat(True)  # disable indicator LEDs
-    gc.collect()
+    return False
 
 
-def get_config(logger):
+#  Saves keys and preferences to sd card
+def save_configuration(logger, config_values):
+    try:
+        with open('/sd/config.txt', 'w') as f:  # save credentials to sd card
+            f.write(config_values['APP_KEY'] + '\r\n' + config_values['APP_EUI'] + '\r\n' +
+                    config_values['interval'] + '\r\n')
+        logger.info('Configuration saved to SD card')
+        return True
+    except:
+        return False
+
+
+#  Reads and returns keys and preferences from sd card
+def read_configuration(logger):
+
+    config_values = {'APP_KEY': None, 'APP_EUI': None, 'interval': None}
+
     if config_filename not in os.listdir('/sd'):
         logger.warning('{} does not exist, failed to configure device'.format(config_filename))
         return
     with open('/sd/' + config_filename, 'r') as f:
         lines = f.readlines()
-        settings = [None]*3
-        settings[0] = lines[0][0:-2]
-        settings[1] = lines[1][0:-2]
-        settings[2] = int(lines[2][0:-2]) * 60  # seconds
-        logger.info("APP_KEY:", settings[0])
-        logger.info("APP_EUI:", settings[1])
-        logger.info("interval:", settings[2], 'seconds')
-        return settings
+        config_values['APP_KEY'] = lines[0][0:-2]
+        config_values['APP_EUI'] = lines[1][0:-2]
+        config_values['interval'] = float(lines[2][0:-2]) * 60  # seconds
+        logger.info('APP_KEY: {}'.format(config_values['APP_KEY']))
+        logger.info('APP_EUI: {}'.format(config_values['APP_EUI']))
+        logger.info('interval: {} seconds'.format(config_values['interval']))
+        return config_values
