15bb939a2da6ee4894008699e36a79389aa52df7
pe-varga <52164948+pe-varga@users.noreply.github.com>
2019-07-18 10:47:33 +0100

    Fix indent in main.py

--- lopy/main.py
+++ lopy/main.py
@@ -34,52 +34,52 @@ try:
     sd = SD()
     os.mount(sd, '/sd')
 
-# Initialise LoggerFactory and loggers
-logger_factory = LoggerFactory()
-status_logger = logger_factory.create_status_logger('status_logger', level=DEBUG, filename='status_log.txt')
-sensor_logger = SensorLogger(filename=path + sensor_name + '.csv.current', terminal_out=True)
+    # Initialise LoggerFactory and loggers
+    logger_factory = LoggerFactory()
+    status_logger = logger_factory.create_status_logger('status_logger', level=DEBUG, filename='status_log.txt')
+    sensor_logger = SensorLogger(filename=path + sensor_name + '.csv.current', terminal_out=True)
 
-# Read configuration file to get preferences
-read_configuration(status_logger)
+    # Read configuration file to get preferences
+    read_configuration(status_logger)
 
-# Check if device is configured, or SD card has been moved to another device
-device_id = hexlify(unique_id()).upper().decode("utf-8")
-if "" in config.values() or config["device_id"] != device_id:
-    reset_configuration(status_logger)
-    #  Forces user to configure device, then reboot
-    _thread.start_new_thread(config_thread, ('Config', status_logger, 300))
+    # Check if device is configured, or SD card has been moved to another device
+    device_id = hexlify(unique_id()).upper().decode("utf-8")
+    if "" in config.values() or config["device_id"] != device_id:
+        reset_configuration(status_logger)
+        #  Forces user to configure device, then reboot
+        _thread.start_new_thread(config_thread, ('Config', status_logger, 300))
 
-# If device is correctly configured continue execution
-else:
-    # Overwrite Preferences - DEVELOPER USE ONLY - keep all overwrites here
-    config["PM1_interval"] = 2
-    config["app_eui"] = APP_EUI
-    config["app_key"] = APP_KEY
+    # If device is correctly configured continue execution
+    else:
+        # Overwrite Preferences - DEVELOPER USE ONLY - keep all overwrites here
+        config["PM1_interval"] = 2
+        config["app_eui"] = APP_EUI
+        config["app_key"] = APP_KEY
 
-    # Delete 'PM1.csv.processing' if it exists TODO: send the content over LoRa instead
-    if PM1_processing in os.listdir():
-        status_logger.info(PM1_processing + 'already exists, removing it')
-        os.remove(PM1_processing)
+        # Delete 'PM1.csv.processing' if it exists TODO: send the content over LoRa instead
+        if PM1_processing in os.listdir():
+            status_logger.info(PM1_processing + 'already exists, removing it')
+            os.remove(PM1_processing)
 
-    # Start 1st PM sensor thread with id: PM1
-    _thread.start_new_thread(pm_thread, (sensor_name, sensor_logger, status_logger))
+        # Start 1st PM sensor thread with id: PM1
+        _thread.start_new_thread(pm_thread, (sensor_name, sensor_logger, status_logger))
 
-    # Start calculating averages and sending data over LoRa
-    PM1_Events = EventScheduler(rtc, logger=status_logger, sensor_name=sensor_name)
+        # Start calculating averages and sending data over LoRa
+        PM1_Events = EventScheduler(rtc, logger=status_logger, sensor_name=sensor_name)
 
-    # Initialise interrupt on user button for configuration over wifi
-    user_button = ButtonPress(logger=status_logger)
-    pin_14 = Pin("P14", mode=Pin.IN, pull=None)
-    pin_14.callback(Pin.IRQ_FALLING | Pin.IRQ_RISING, user_button.press_handler)
+        # Initialise interrupt on user button for configuration over wifi
+        user_button = ButtonPress(logger=status_logger)
+        pin_14 = Pin("P14", mode=Pin.IN, pull=None)
+        pin_14.callback(Pin.IRQ_FALLING | Pin.IRQ_RISING, user_button.press_handler)
 
-    status_logger.info("Initialisation finished")
-    # Blink green twice and put the heartbeat back to identify that the device has been initialised
-    for i in range(2):
-        pycom.rgbled(0x000000)
-        time.sleep(0.5)
-        pycom.rgbled(0x005500)
-        time.sleep(0.5)
-    pycom.heartbeat(True)
+        status_logger.info("Initialisation finished")
+        # Blink green twice and put the heartbeat back to identify that the device has been initialised
+        for i in range(2):
+            pycom.rgbled(0x000000)
+            time.sleep(0.5)
+            pycom.rgbled(0x005500)
+            time.sleep(0.5)
+        pycom.heartbeat(True)
 except Exception as e:
     print(e)
     pycom.rgbled(0x770000)
