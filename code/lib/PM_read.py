da2392fcb639f8e14c61a4c33e639de974de6b2f
ossonts <472243+sossont@users.noreply.github.com>
2021-07-19 11:11:14 +0100

    Inline the welfords calculation for ONE var -- slimline

--- code/lib/PM_read.py
+++ code/lib/PM_read.py
@@ -8,9 +8,11 @@ import time
 from WelfordAverage import WelfordAverage
 from Constants import PM_SENSOR_SAMPELING_RATE , PM_SAMPLE_COUNT_FOR_AVERAGE
 
-#This is important code, keep it fast are reliable. 
+#This is important code, keep it FAST are reliable. 
+#Note syntax issues here may look like cannot read sensors
 #Entry point for thread (method only not class) 
 def pm_thread(sensor_name,config,  debugLogger, pins, serial_id):
+    #Thread creates object and keeps it always.-- object triggers alarms. -- one per PM sensor
     rdr = PMSensorReader(sensor_name,config,  debugLogger, pins, serial_id)
 
 
@@ -31,22 +33,20 @@ class PMSensorReader:
         self.debugLogger = debugLogger
         self.sensor_name = sensor_name
         self.debugLogger.debug("Thread {} started".format(sensor_name))
-        #self.chrono = Timer.Chrono()
-        #Welfords variables -- keep average of these 
+        
+        #Welford is now inline and not an object to save memory. 
+        #Use welford here : https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
         self.welfordsCount = 0
         self.welfordsMean = 0
         self.welfordsM2 = 0        
-        #self.gr03umAverage = WelfordAverage(debugLogger)
-        #self.gr05umAverage = WelfordAverage(debugLogger)
-        #self.gr10umAverage = WelfordAverage(debugLogger)
-        #averages = [gr03umAverage,gr05umAverage,gr10umAverage] # list to pass single param, and not dict for speed
-        #self.sampleCounter = 0
 
-
+        #Logging files (sensor data and average senor data)
         self.sensor_logger = SensorLogger(sensor_name=sensor_name, terminal_out=True) 
         self.averageLogger = SensorLogger(sensor_name=sensor_name+"Average", terminal_out=True)
         
+        # Name PM1 - > Plantower via config.
         self.sensor_type = config.get_config(sensor_name)
+        # Wait for sensor to poer up time from config
         self.init_time = int(config.get_config(sensor_name + "_init"))
 
         init_count = 0
@@ -54,8 +54,7 @@ class PMSensorReader:
             # initialise sensor
             self.sensor = Plantower(pins=pins, id=serial_id)
             time.sleep(1)
-            #TODO: why is this like this, why not sleep for init_time -- do i need to clear the serial buffer?
-            # warm up time  - readings are not logged
+            
             while init_count < self.init_time:
                 try:
                     time.sleep(1)
@@ -88,45 +87,8 @@ class PMSensorReader:
                     blink_led((0x550000, 0.4, True))
 
         # start a periodic timer interrupt to poll readings every second
-
-
-        #Use welford here : https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-        #Average variables? -- pass them in
-        #TODO: Hold on do the sensirion and plantower return the same strings????
-        #processing_alarm = Timer.Alarm(process_readings, arg=(sensor_type, sensor, sampleCounter, averages, sensor_logger,  averageLogger ,debugLogger), s=PM_SENSOR_SAMPELING_RATE, periodic=True)
-
         debugLogger.debug("Starting repeating alarm/timer")
         self.__processing_alarm = Timer.Alarm(self.process_readings, PM_SENSOR_SAMPELING_RATE, periodic=True)
-
-        #DO averaging here? -- timer
-        # TOO heavy processing_averages = Timer.Alarm(process_averages, arg=(averages,averageLogger,debugLogger), s=10, periodic=True)
-
-
-    # def process_averages(self):
-    #     try:
-
-    #         #averages, averageLogger, debugLogger = args[0], args[1], args[2]
-
-    #         gr03umCount, gr03umMean, gr03umVariance, gr03umSampleVariance = averages[0].getAverageAndReset()
-    #         #gr05umCount, gr05umMean, gr05umVariance, gr05umSampleVariance = averages[1].getAverageAndReset()
-    #         #gr10umCount, gr10umMean, gr10umVariance, gr10umSampleVariance = averages[2].getAverageAndReset()
-
-    #         #log to file
-    #         if(gr03umCount == gr03umCount == gr03umCount):
-
-    #             averageLogger.log_row("".join([str(gr03umMean)])) # , "," ,str(gr05umMean) , "," ,str(gr10umMean), "," , str(gr10umCount)]))
-    #         else: 
-    #             averageLogger.log_row("".join([str(gr03umMean) , "," ,str(gr05umMean) , "," ,str(gr10umMean) , "," , str(gr03umCount) , "," , str(gr05umCount) , "," , str(gr10umCount)]))
-            
-    #         #add to transmit?
-    #     except Exception as e:
-    #         debugLogger.error("Calculate average failed.")
-    #         debugLogger.info(e)
-        
-    #         blink_led((0x550000, 0.4, True))
-    
-        
-
 
     def process_readings(self,alarm):
         """
@@ -135,42 +97,15 @@ class PMSensorReader:
         :type args: str, str, SensorLogger object, LoggerFactory object
         """
         
-        #print(alarm)
-        #print(print(alarm.read_ms())) 
-        #self.chrono.reset()
-        #self.chrono.start()
-        #sensor_type, sensor,sampleCounter , averages, sensor_logger, averageLogger,debugLogger   = args[0], args[1], args[2], args[3] ,  args[4] ,  args[5] ,  args[6]   #TODO: this looks clunky and in need of a fix
-        #print("START")
         #Must run 1Hz !
         try:
-            #print(self.sensor)
             recv = self.sensor.read() #This string should be fine to just log, but we do need some values to average
-            #print ("HERE1111" )
-            #print ("HERE1111" +  str(recv) )
             if recv:
                 revStr = str(recv) 
                 recvList = revStr.split(',')
-                #print ("HERE")
-                #curr_timestamp = recv_lst[0]
-                #sensor_reading_float = [float(i) for i in recv_lst[1:]]
-                #sensor_reading_round = [round(i) for i in sensor_reading_float]  #TODO: this looks VERY processor intensive (remember 1hz here! x number of sensors)
-                #lst_to_log = [curr_timestamp] + [str(i) for i in sensor_reading_round] #TODO: check that round is sane here
-                #line_to_log = ','.join(lst_to_log)
-                
-                #self.sensor_logger.log_row(line_to_log)
-                
-                
                 
                 self.sensor_logger.log_row(revStr)
                 
-                ##attempt averaging
-                #ROWS: "timestamp", "pm10_cf1", "PM1", "pm25_cf1", "PM25", "pm100_cf1", "PM10", "gr03um", "gr05um", "gr10um", "gr25um", "gr50um", "gr100um", ""
-                #IN ORDER "gr03um", "gr05um", "gr10um"
-                
-                #self.gr03umAverage.update(round(float(recvList[8])))
-                #self.gr05umAverage.update(round(float(recvList[9])))
-                #self.gr10umAverage.update(round(float(recvList[10])))
-
                 #==============Single welfords average=========
                 #Average gr03umAverage = float(recvList[8])
                 grumValue = float(recvList[8])  ## Value to average (we only average one)
@@ -181,35 +116,17 @@ class PMSensorReader:
                 self.welfordsM2 += ( delta * delta2)
                 #====End welfords==============
 
-                 
-                print(self.welfordsCount)
-
                 if (self.welfordsCount >= PM_SAMPLE_COUNT_FOR_AVERAGE): #Keep adding data
 
                     variance = self.welfordsM2/self.welfordsCount
-                    sampleVariance = self.welfordsM2 / (self.welfordsCount -1)
-                    #self.welfordsCount, self.mean , variance, sampleVariance)
-                    self.averageLogger.log_row(str(self.welfordsMean)) # , "," ,str(gr05umMean) , "," ,str(gr10umMean), "," , str(gr10umCount)]))
+                    sampleVariance = self.welfordsM2 / (self.welfordsCount -1)                                        
                     self.averageLogger.log_row("".join([str(self.welfordsCount) , "," ,str(self.welfordsMean) , "," ,str(sampleVariance), "," , str(variance)]))
 
-                    #Reset
+                    #Reset welfords
                     self.welfordsCount = 0
                     self.welfordsMean = 0
                     self.welfordsM2 = 0
-
-
-
-                    # gr03umCount, gr03umMean, gr03umVariance, gr03umSampleVariance = self.averages[0].getAverageAndReset()
-                    # gr05umCount, gr05umMean, gr05umVariance, gr05umSampleVariance = self.averages[1].getAverageAndReset()
-                    # gr10umCount, gr10umMean, gr10umVariance, gr10umSampleVariance = self.averages[2].getAverageAndReset()
-                    # if(gr03umCount == gr03umCount == gr03umCount):
-                    #     self.averageLogger.log_row("".join([str(gr03umMean)])) # , "," ,str(gr05umMean) , "," ,str(gr10umMean), "," , str(gr10umCount)]))
-                    # else: 
-                    #     self.averageLogger.log_row("".join([str(gr03umMean) , "," ,str(gr05umMean) , "," ,str(gr10umMean) , "," , str(gr03umCount) , "," , str(gr05umCount) , "," , str(gr10umCount)]))
-                    
-
-            #self.chrono.stop()
-            #print(print(alarm.read_ms())) #get an idea of time 0.0827
+            # #get an idea of time 0.0827
         except Exception as e:
             self.debugLogger.error("Failed to read from sensor {}".format(self.sensor_type))
             self.debugLogger.debug(e)
